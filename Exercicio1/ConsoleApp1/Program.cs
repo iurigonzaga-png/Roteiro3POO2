872eccf02a491a53e24fbfc6ed7d7a2b256df5aa
iurigonzaga-png <iuri.gonzaga@esamcuberlandia.com.br>
2026-09-01 21:32:23 -0300

    Add files via upload

---
+++ Exercicio1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ExerciciosJson
+{
+    public class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Ano { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro = new Livro
+            {
+                Titulo = "Direito Constitucional",
+                Autor = "Alexandre de Moraes",
+                Ano = 1997
+            };
+            var opcoes = new JsonSerializerOptions { WriteIndented = true };
+            string jsonLivro = JsonSerializer.Serialize(livro, opcoes);
+            Console.WriteLine(jsonLivro);
+        }
+    }
+}
