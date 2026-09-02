40e309e897d5d1ab7fc7b1aae0b2e2f98d3d4016
iurigonzaga-png <iuri.gonzaga@esamcuberlandia.com.br>
2026-09-01 21:32:52 -0300

    Add files via upload

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace ExerciciosJson
+{
+
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+        public int Idade { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = @"C:\Users\alunolab\Desktop\Roteiro 3 POO2\Exercicio2\aluno.json";
+            
+
+            try
+            {
+                string jsonLido = File.ReadAllText(caminhoArquivo);
+
+                Aluno aluno = JsonSerializer.Deserialize<Aluno>(jsonLido);
+
+                Console.WriteLine("Dados do Aluno lidos com sucesso:");
+                Console.WriteLine($"Nome:  {aluno.Nome}");
+                Console.WriteLine($"Curso: {aluno.Curso}");
+                Console.WriteLine($"Idade: {aluno.Idade} anos");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Erro: O arquivo '{caminhoArquivo}' não foi encontrado.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro ao ler o arquivo: {ex.Message}");
+            }
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
+
