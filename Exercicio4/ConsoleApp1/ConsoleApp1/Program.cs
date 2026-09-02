1135043fc164231960dec3e4ac5eb25c3bd875f4
iurigonzaga-png <iuri.gonzaga@esamcuberlandia.com.br>
2026-09-01 21:33:53 -0300

    Add files via upload

---
+++ Exercicio4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string jsonConfig = @"{
+            'Servidor': '192.168.0.1',
+            'Porta': 1433,
+            'Usuario': 'admin'
+        }";
+
+        JObject config = JObject.Parse(jsonConfig);
+
+        config["Porta"] = 8080;
+
+        string jsonModificado = config.ToString();
+
+        Console.WriteLine(jsonModificado);
+    }
+}
